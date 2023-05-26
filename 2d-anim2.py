0ea058c4aa8f5fb9ad89b2e3eb3c0a6365691548
Justus <Justus.Beck@campus.lmu.de>
2023-05-26 11:55:26 +0200

    fancy moving bar

--- 2d-anim2.py
+++ 2d-anim2.py
@@ -22,23 +22,35 @@ duration = 2.0
 start_y = 20
 start_x = 200
 # distance to move in x direction (positive or negative)
-shift = 100
+x_coordinate = 100
 # size of rectangle
 rect_size = (20,400)
-rect_color = (255,255,255)
+rect_color = (128, 128, 128)
 
 # Create the rectangle
 rect = pg.Rect(start_x, start_y, *rect_size)
 
+# Name of the text file to read the shift value from
+shift_file_name = "shift.txt"
+
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    #screen.fill((128,128,128))
+    # Check the text file for a new shift value
+    try:
+        with open(shift_file_name, 'r') as f:
+            new_x_coordinate = int(f.read().strip())
+            if new_x_coordinate != x_coordinate:
+                x_coordinate = new_x_coordinate
+                start_x = rect.x
+                # calculate the shift to get to the new x coordinate
+                shift = x_coordinate - start_x
+                start_time = time.time()
+    except (FileNotFoundError, ValueError):
+        pass  # If the file doesn't exist or the content is not a number, ignore it
 
     # Calculate the current time elapsed since the start of the animation
     t = (time.time() - start_time) / duration
@@ -46,12 +58,17 @@ while running:
     # If the animation is not over, move the rectangle
     if t < 1.0:
         move_x = int(shift * t)
+        old_rect = rect.copy()  # Keep a copy of the old rectangle
         rect.x = start_x + move_x
-    #else:
-    #    start_time = time.time()
-
+        # Fill the old rectangle with the background color
+        screen.fill((0, 0, 0))
+        pg.draw.rect(screen, rect_color, old_rect)
+    else:
+        # Reset the animation if it's over
+        #start_time = time.time()
+        pass
     # plot rectangle in current position
-    pg.draw.rect(screen, rect_color, rect)
+    #pg.draw.rect(screen, rect_color, rect)
 
     # flip() the display to put your work on screen
     pg.display.flip()
